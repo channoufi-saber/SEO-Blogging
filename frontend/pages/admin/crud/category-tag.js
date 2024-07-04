abea46ec07285a3f065d0ef25f89c0648176c420
channoufi-saber <saber.channoufi@esprit.tn>
2024-07-04 14:40:56 +0100

    Categories and Tags - Frontend

---
+++ frontend/pages/admin/crud/category-tag.js
@@ -0,0 +1,29 @@
+import Admin from "../../../components/auth/Admin";
+import Category from "../../../components/crud/Category";
+import Tag from "../../../components/crud/Tag";
+import Layout from "../../../components/Layout";
+
+const CategoryTag = () => {
+	return (
+		<Layout>
+			<Admin>
+				<div className="container-fluid">
+					<div className="row">
+						<div className="col-md-12 pt-5 pb-5">
+							<h2>Manage Categories and Tags</h2>
+						</div>
+						<div className="col-md-6">
+							<Category />
+						</div>
+						<div className="col-md-6">
+							<Tag />
+						</div>
+					</div>
+				</div>
+			</Admin>
+		</Layout>
+
+	)
+}
+
+export default CategoryTag;
--- frontend/pages/admin/index.js
+++ frontend/pages/admin/index.js
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import Admin from "../../components/auth/Admin";
 import Layout from "../../components/Layout";
 
@@ -5,7 +6,21 @@ const AdminIndex = () => {
     return (
         <Layout>
             <Admin>
-                <h2>Admin Dashboard</h2>
+                <div className="container-fluid">
+                    <div className="row">
+                        <h2>Admin Dashboard</h2>
+                    </div>
+                    <div className="col-md-4">
+                        <ul className="list-group" >
+                            <li className="list-group-item">
+                                <Link href="/admin/crud/category-tag">
+                                    <a>Create Category</a>
+                                </Link>
+                            </li>
+                        </ul>
+                    </div>
+                    <div className="col-md-8">right</div>
+                </div>
             </Admin>
         </Layout>
     )
