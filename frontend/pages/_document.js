f073ce299915dca5447a93ffbb872c89de4e29b5
channoufi-saber <saber.channoufi@esprit.tn>
2024-09-08 12:08:40 +0100

    Google Analytics

--- frontend/pages/_document.js
+++ frontend/pages/_document.js
@@ -1,9 +1,25 @@
 import Document, { Head, Html, Main, NextScript } from 'next/document';
 
 class MyDocument extends Document {
-    render() {
-        return (
-            <Html lang="en">
+  setGoogleTags() {
+
+
+    return {
+      __html: `
+        window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-WQSKBZL2GY');
+
+        `
+    };
+        
+  }
+  
+  render() {
+    return (
+      <Html lang="en">
         <Head>
           <meta charSet="UTF-8" />
           <meta name="viewport" content="width=device-width, initial-scale=1.0" />
@@ -17,14 +33,16 @@ class MyDocument extends Document {
             href="https://cdnjs.cloudflare.com/ajax/libs/nprogress/0.2.0/nprogress.min.css"
           />              
           <link rel="stylesheet" href="/static/css/styles.css" />
+          <script dangerouslySetInnerHTML={this.setGoogleTags()} />
         </Head>
         <body>
+          <script async src="https://www.googletagmanager.com/gtag/js?id=G-WQSKBZL2GY"></script>            
           <Main />
           <NextScript />
         </body>
       </Html>
-        );
-    }
+    );
+  }
 }
 
 export default MyDocument;
